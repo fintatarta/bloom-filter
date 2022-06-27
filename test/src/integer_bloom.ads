52ee7ea4ee9a4e977a41758bfe8734d9557b306d
Riccardo <framefritti@gmail.com>
2022-06-27 12:36:44 +0200

    Basic test bloom filter OK

--- test/src/integer_bloom.ads
+++ test/src/integer_bloom.ads
@@ -1,7 +1,4 @@
 with Bloom_Filters;
-package Integer_Bloom  is
-   pragma SPARK_Mode (On);
+package Integer_Bloom is
+  new Bloom_Filters (Integer);
 
-   package pippo is
-     new Bloom_Filters (Integer);
-end Integer_Bloom;
--- test/src/test_bloom.adb
+++ test/src/test_bloom.adb
@@ -1,6 +1,18 @@
-procedure Test_Bloom is
+with Ada.Text_IO; use Ada.Text_IO;
+with Integer_Bloom;
 
+procedure Test_Bloom is
+   use Integer_Bloom;
+   Filter : Bloom_Filter := Create (12, 4);
 begin
-   --  Insert code here.
-   null;
+   Add (Filter, 7);
+   Add (Filter, 12345678);
+   Add (Filter, -33);
+
+   Put_Line (Boolean'Image (Contains (Filter, 7)));
+   Put_Line (Boolean'Image (not Contains (Filter, 17)));
+
+   Put_Line (Boolean'Image (not Contains (Filter, 32471)));
+
+   Put_Line (Boolean'Image (Contains (Filter, -33)));
 end Test_Bloom;
